15d9a414056475ed94db5cb2b7a2d5563e666b7a
rcahuata-svg <rcahuata@gmail.com>
2026-05-12 19:48:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicacion en Python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Ramiro Cahuata")
+
+sesion = st.selectbox("Seleccione una sesión"),["Sesión 1","Sesión2"]

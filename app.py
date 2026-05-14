712ad221812843b80f85520e9bf0981291dc6722
rcahuata-svg <rcahuata@gmail.com>
2026-05-13 21:23:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ st.write("Elaborado por: Ramiro Cahuata")
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
 
 Sesión = st.sidebar.selectbox("Seleccione una sesion", ["seccion1","seccion2","seccion3"])
+
+if sesion == "Sesión 1":
+
+  st.write("Bienvenido la sesión 1")
+
+  st.image("Python_logo.png" )
+
+ 
+
+elif sesion == "Sesión 2":
+
+  st.write("Bienvenido la sesión 2")

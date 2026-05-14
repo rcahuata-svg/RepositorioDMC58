f20c1cfc1279649e08398a3bd9d933a253125577
rcahuata-svg <rcahuata@gmail.com>
2026-05-13 21:18:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ st.write("Elaborado por: Ramiro Cahuata")
 
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
 
-Sesión = st.selectbox("Seleccione una sesion",["seccion1","seccion2","seccion3"]
+Sesión = st.selectbox("Seleccione una sesion", ["seccion1","seccion2","seccion3"])

44b88b1997e57796f102c4959d5029045c2eaf5e
rcahuata-svg <rcahuata@gmail.com>
2026-05-14 21:11:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,9 +48,7 @@ elif sesion == "Sesión 2":
 
   st.write("El precio final del producto es: ", precio_final_producto  )
 
- 
 
- 
 
  
 
@@ -58,24 +56,16 @@ elif sesion == "Sesión 3":
 
   st.write("Bienvenido la sesión 3")
 
-inicio_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+fin_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
 
+  arreglo = np.arange(0 , fin_rango)
 
-
-
-
-
-
-
-
-
-
-
-
+st.write(arreglo)
  
 
 else:
 
   st.write("Bienvenido la sesión 4")
+
 
 

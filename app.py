309b95773e9394c86ee30b18c11f1c02bdbf77ec
rcahuata-svg <rcahuata@gmail.com>
2026-05-14 21:20:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,43 @@
 import streamlit as st
-
- 
+import numpy as np
 
 st.title("Mi primera aplicación en python")
 
- 
-
 st.sidebar.title("Parámetros")
 
- 
-
-st.write("Elaborado por: Carlos Carrillo")
-
- 
+st.write("Elaborado por: Ramiro")
 
 st.sidebar.image("Antapaccay.png")
 
- 
-
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
 
- 
-
 if sesion == "Sesión 1":
-
   st.write("Bienvenido la sesión 1")
-
   st.image("Python_logo.png" )
 
- 
-
 elif sesion == "Sesión 2":
-
   st.write("Bienvenido la sesión 2")
 
- 
-
   precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value = 1200)
-
   descuento = st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100 )
 
- 
-
   precio_final_producto = precio - (precio*(descuento/100))
-
- 
 
   st.write("El precio final del producto es: ", precio_final_producto  )
 
 
-
- 
-
 elif sesion == "Sesión 3":
-
   st.write("Bienvenido la sesión 3")
-
-fin_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+  
+  fin_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
 
   arreglo = np.arange(0 , fin_rango)
 
-st.write(arreglo)
- 
+  st.write(arreglo)
+
+
 
 else:
-
   st.write("Bienvenido la sesión 4")
-
 
 

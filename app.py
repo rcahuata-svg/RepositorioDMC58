367f70614c9244ccdbd95ea41a4e23ef7428d39f
rcahuata-svg <rcahuata@gmail.com>
2026-05-14 21:09:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,20 @@ elif sesion == "Sesión 3":
 
   st.write("Bienvenido la sesión 3")
 
+inicio_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
 else:

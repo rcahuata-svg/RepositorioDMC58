4a6c383ac0ff4b94bf2f2b9447f012d64f385bf0
rcahuata-svg <rcahuata@gmail.com>
2026-05-14 22:04:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Ramiro")
 
-st.sidebar.image("Antapaccay.png")
+st.sidebar.image("DMC.png")
 
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
 
